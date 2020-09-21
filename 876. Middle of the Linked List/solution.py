c21e5d3b30b50145231df2e7c78d1ad842dad26b
Wang Hao <goinfrerie@gmail.com>
2020-09-21 21:28:30 +0800

    modified 876, IDE
    Just tried out solving problems using IDE

--- 876. Middle of the Linked List/solution.py
+++ 876. Middle of the Linked List/solution.py
@@ -1,11 +1,23 @@
-# Definition for singly-linked list.
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-        
-class Solution:
-    def middleNode(self, head: ListNode) -> ListNode:
+import argparse
+def main(arr):
+    class ListNode:
+        def __init__(self, val=0, next=None):
+            self.val = val
+            self.next = next
+
+    def arr2lst(arr):
+        '''
+        convert array to linked list
+        :param arr:
+        :return:
+        '''
+        dummy = curr = ListNode(0)
+        for e in arr:
+            curr.next = ListNode(e)
+            curr = curr.next
+        return dummy.next
+
+    def middleNode(head: ListNode) -> ListNode:
         #create 2 pointers, fast is 2 times speed of slow
         slow = fast = head
         while fast and fast.next:
@@ -13,4 +25,12 @@ class Solution:
             slow = slow.next
             fast = fast.next.next
         return slow
-        
+
+    head = arr2lst(arr)
+    print(middleNode(head).val)
+
+    return None
+
+if __name__ == "__main__":
+    arr = [1,5,6,3,3,4,6,8]
+    main(arr)
